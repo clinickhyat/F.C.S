04cac10c21251e5b6c478a5ac61dadf90a4e12b9
clinickhyat <abdulrahmanahmedalkhayat@gmail.com>
2026-07-17 15:33:56 +0300

    Update client.ts

--- fathclinicsmart-dev-supabase/src/integrations/supabase/client.ts
+++ fathclinicsmart-dev-supabase/src/integrations/supabase/client.ts
@@ -1,12 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-// استخدام القيم الصحيحة مباشرة لضمان الاتصال
-const SUPABASE_URL = "https://ebuumyqofptnjjaujlyd.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVidXVteXFvZnB0bmpqYXVqbHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDM1MjcsImV4cCI6MjA5OTE3OTUyN30.2cbgowu9hhqdNl5eOS-9VqykaR270-7BDnwtHvOcY9k";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || '';
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
-  console.error('Supabase URL or Key is missing.');
+  console.warn('Supabase URL or Anon Key is missing.');
 }
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
@@ -16,4 +15,3 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
     autoRefreshToken: true,
   }
 });
-
